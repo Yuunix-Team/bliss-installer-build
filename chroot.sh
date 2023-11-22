50d04656f46d8b35b228ea37d5de608edab20819
shadichy <hotrobb360web@gmail.com>
2023-11-22 11:50:41 +0700

    changes on chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -4,21 +4,8 @@ export PATH=/bin:/sbin:/usr/local/bin:/usr/bin:/usr/sbin
 
 mkdir -p android apex boot data gearroot gearload system vendor linkerconfig
 
-# cd /lib
-# for so in *; do
-# 	if [ -h "/usr/lib/$so" ] || [ ! -e "/usr/lib/$so" ]; then
-# 		cp -rf /lib/"$so" /usr/lib/ || true
-# 	fi
-# done
+echo 'localhost' > /etc/hostname
 
-# for d in bin sbin usr/sbin; do
-# 	cd /$d
-# 	for b in *; do
-# 		if [ -h "/usr/bin/$b" ] || [ ! -e "/usr/bin/$b" ]; then
-# 			busybox mv -f /$d/"$b" /usr/bin/ || true
-# 		fi
-# 	done
-# done
+gpg --batch --passphrase '' --quick-gen-key 'root@localhost' default default
 
-# shellcheck disable=SC2016
-# busybox sed -i 's|GRUB_DEVICE="`${grub_probe} --target=device /`"|GRUB_DEVICE="`${grub_probe} --target=device "$ROOT"`"|g' /usr/bin/grub-mkconfig
+abuild-keygen --append
